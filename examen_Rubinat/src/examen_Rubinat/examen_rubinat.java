7ba5244c726ddfc3d9707ada1496f6a3a59c771c
JRubinat <jrubinat@hotmail.com>
2018-02-27 10:46:06 +0100

    segona pujada

--- examen_Rubinat/src/examen_Rubinat/examen_rubinat.java
+++ examen_Rubinat/src/examen_Rubinat/examen_rubinat.java
@@ -4,13 +4,10 @@ public class examen_rubinat {
 	public static void main (String[] args) {
 		printOne();
 		printOne();
-		printTwo()
+		
 	}
 	public static void printOne() {
 		System.out.println("Hello World"); 
 	}
-	public static void printTwo();{
-		printOne();
-		printOne();
-	}
+	
 }
