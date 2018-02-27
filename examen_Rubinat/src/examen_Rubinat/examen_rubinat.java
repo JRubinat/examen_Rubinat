c5b37b9275fc3e322331255d63bd9c2663af9c27
JRubinat <jrubinat@hotmail.com>
2018-02-27 10:42:52 +0100

    primera pujada

---
+++ examen_Rubinat/src/examen_Rubinat/examen_rubinat.java
@@ -0,0 +1,16 @@
+package examen_Rubinat; 
+
+public class examen_rubinat {
+	public static void main (String[] args) {
+		printOne();
+		printOne();
+		printTwo()
+	}
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+	public static void printTwo();{
+		printOne();
+		printOne();
+	}
+}
